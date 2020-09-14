298ab314eac1331c2144d72d5ed6e7b5189b268f
zhaohengyuan1 <hubylidayuan@gmail.com>
2020-09-14 15:30:03 +0800

    update

--- codes/models/networks.py
+++ codes/models/networks.py
@@ -13,9 +13,11 @@ def define_G(opt):
     if which_model == 'PAN':
         netG = PAN_arch.PAN(in_nc=opt_net['in_nc'], out_nc=opt_net['out_nc'],
                                        nf=opt_net['nf'], unf=opt_net['unf'], nb=opt_net['nb'], scale=opt_net['scale'])
-    elif which_model == 'SRResNet_PA':
-        netG = SRResNet_arch.SRResNet_PA(in_nc=opt_net['in_nc'], out_nc=opt_net['out_nc'],
+    elif which_model == 'MSRResNet_PA':
+        netG = SRResNet_arch.MSRResNet_PA(in_nc=opt_net['in_nc'], out_nc=opt_net['out_nc'],
                                        nf=opt_net['nf'], nb=opt_net['nb'], upscale=opt_net['scale'])
+    elif which_model == 'RCAN_PA':
+        netG = RCAN_arch.RCAN_PA(n_resgroups=opt_net['n_resgroups'], n_resblocks=opt_net['n_resblocks'], n_feats=opt_net['n_feats'], res_scale=opt_net['res_scale'], n_colors=opt_net['n_colors'], rgb_range=opt_net['rgb_range'], scale=opt_net['scale'])
     else:
         raise NotImplementedError('Generator model [{:s}] not recognized'.format(which_model))
 
